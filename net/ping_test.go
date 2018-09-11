82ae32b9666a8f71762f2bf98636cf59f3e31788
Yi Tang <ssnailtang@gmail.com>
2018-09-12 00:19:02 +0800

    fix: ping test

--- net/ping_test.go
+++ net/ping_test.go
@@ -4,26 +4,34 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
+
+	"github.com/yittg/ving/net/protocol"
 )
 
 func Test(t *testing.T) {
-	p := NewPing()
+	stopChan := make(chan bool, 2)
+	p := NewPing(stopChan)
 	if err := p.Start(); err != nil {
 		t.Error("start ping error", err)
 	}
 
 	targets := []string{"127.0.0.1", "example.com"}
 
+	networkTargets := make([]*protocol.NetworkTarget, 0, len(targets))
+	for _, t := range targets {
+		networkTargets = append(networkTargets, protocol.ResolveTarget(t))
+	}
 	wg := sync.WaitGroup{}
-	for _, target := range targets {
+	for _, target := range networkTargets {
 		wg.Add(1)
-		go func(addr string) {
+		go func(t *protocol.NetworkTarget) {
 			defer wg.Done()
-			duration, err := p.PingOnce(addr)
+			duration, err := p.PingOnce(t, time.Second)
 			if err != nil {
-				fmt.Printf("ping %s error, %v\n", addr, err)
+				fmt.Printf("ping %s error, %v\n", t.Raw, err)
 			} else {
-				fmt.Printf("ping %s ttl = %v\n", addr, duration)
+				fmt.Printf("ping %s ttl = %v\n", t.Raw, duration)
 			}
 		}(target)
 	}
