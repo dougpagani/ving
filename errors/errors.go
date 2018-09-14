f5f0c706a056c78a24ccf8acd5265dd8998529f4
Yi Tang <ssnailtang@gmail.com>
2018-09-15 00:58:35 +0800

    net: implement trace with icmp

--- errors/errors.go
+++ errors/errors.go
@@ -8,6 +8,14 @@ func (*ErrTimeout) Error() string {
 	return "timeout"
 }
 
+// ErrTTLExceed for ttl exceed
+type ErrTTLExceed struct {
+}
+
+func (*ErrTTLExceed) Error() string {
+	return "ttl exceed"
+}
+
 // ErrNoTarget represents wrong usage
 type ErrNoTarget struct {
 }
