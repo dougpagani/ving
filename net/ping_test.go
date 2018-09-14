8171c36abc1e77a068f7b5d55e9c3f8cf88e1c07
Yi Tang <ssnailtang@gmail.com>
2018-09-14 21:10:19 +0800

    *: use example in ping test

--- net/ping_test.go
+++ net/ping_test.go
@@ -1,19 +1,18 @@
 package net
 
 import (
-	"fmt"
+	"log"
 	"sync"
-	"testing"
 	"time"
 
 	"github.com/yittg/ving/net/protocol"
 )
 
-func Test(t *testing.T) {
+func Example() {
 	stopChan := make(chan bool, 2)
 	p := NewPing(stopChan)
 	if err := p.Start(); err != nil {
-		t.Error("start ping error", err)
+		log.Fatalf("start nping failed, %v", err)
 	}
 
 	targets := []string{"127.0.0.1", "example.com"}
@@ -29,11 +28,13 @@ func Test(t *testing.T) {
 			defer wg.Done()
 			rtt, err := p.PingOnce(t, time.Second)
 			if err != nil {
-				fmt.Printf("ping %s error, %v\n", t.Raw, err)
+				log.Printf("ping %s error, %v\n", t.Raw, err)
 			} else {
-				fmt.Printf("ping %s rtt = %v\n", t.Raw, rtt)
+				log.Printf("ping %s rtt = %v\n", t.Raw, rtt)
 			}
 		}(target)
 	}
 	wg.Wait()
+
+	// Output:
 }
