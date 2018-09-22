b550b6ae56121b44b023a259c21c42c77712fbf5
Yi Tang <ssnailtang@gmail.com>
2018-09-22 17:41:10 +0800

    *: load add-on in main package

---
+++ addons.go
@@ -0,0 +1,5 @@
+package main
+
+import (
+	_ "github.com/yittg/ving/addons/trace"
+)
--- core/addons.go
+++
@@ -1,6 +0,0 @@
-package core
-
-import (
-	// import all add-ons to register to registry
-	_ "github.com/yittg/ving/addons/trace"
-)
