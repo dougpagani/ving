a32b0de8d9ac1ba4e4f58e079fa1894471df22ca
Yi Tang <ssnailtang@gmail.com>
2018-10-12 21:43:42 +0800

    opt: provide shorthand option for trace

--- options/options.go
+++ options/options.go
@@ -39,14 +39,14 @@ type Option struct {
 func allPortNumber(strs ...string) ([]int, error) {
 	ns := make([]int, 0, len(strs))
 	for _, s := range strs {
-		if n, err := strconv.ParseInt(s, 10, 64); err != nil {
+		n, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
 			return nil, err
-		} else {
-			if n <= 0 || n > 65535 {
-				return nil, &errors.ErrInvalidPort{}
-			}
-			ns = append(ns, int(n))
 		}
+		if n <= 0 || n > 65535 {
+			return nil, &errors.ErrInvalidPort{}
+		}
+		ns = append(ns, int(n))
 	}
 	return ns, nil
 }
@@ -58,19 +58,19 @@ func (o *Option) portsValid() bool {
 				return false
 			}
 			pRangeS := strings.Split(p, "-")
-			if pRange, err := allPortNumber(pRangeS[0], pRangeS[1]); err != nil {
+			pRange, err := allPortNumber(pRangeS[0], pRangeS[1])
+			if err != nil {
 				return false
-			} else {
-				for x := pRange[0]; x <= pRange[1]; x++ {
-					o.MorePorts = append(o.MorePorts, x)
-				}
+			}
+			for x := pRange[0]; x <= pRange[1]; x++ {
+				o.MorePorts = append(o.MorePorts, x)
 			}
 		} else {
-			if port, err := allPortNumber(p); err != nil {
+			port, err := allPortNumber(p)
+			if err != nil {
 				return false
-			} else {
-				o.MorePorts = append(o.MorePorts, port...)
 			}
+			o.MorePorts = append(o.MorePorts, port...)
 		}
 	}
 	return len(o.MorePorts) <= 1024
@@ -88,9 +88,9 @@ func ParseCommandLine(opt *Option) []string {
 	flag.DurationVarP(&opt.Interval, "interval", "i", time.Second, "ping interval, must >=10ms")
 	flag.DurationVarP(&opt.Timeout, "timeout", "t", time.Second, "ping timeout, must >=10ms")
 	flag.BoolVarP(&opt.Gateway, "gateway", "g", false, "ping gateway")
-	flag.BoolVarP(&opt.Trace, "trace", "", false, "traceroute the target after start")
-	flag.BoolVarP(&opt.Ports, "ports", "", false, "touch the target ports after start")
-	flag.StringArrayVarP(&opt.MorePortsStr, "more-ports", "P", nil,
+	flag.BoolVarP(&opt.Trace, "trace", "T", false, "automatically traceroute the target")
+	flag.BoolVarP(&opt.Ports, "ports", "", false, "automatically probe the target ports")
+	flag.StringArrayVarP(&opt.MorePortsStr, "more-ports", "P", []string{},
 		"ports to probe, e.g. -P 8080 -P 8082-8092, no more than #1024 ports")
 	flag.BoolVarP(&opt.Sort, "sort", "", false, "sort by statistic")
 	flag.BoolVarP(&opt.ShowVersion, "version", "v", false, "display the version")
