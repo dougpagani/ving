9c023c3dfe3b81016c7b95a24f9d684fc208aedb
Yi Tang <ssnailtang@gmail.com>
2018-10-21 20:05:00 +0800

    config: change ui config field name

--- ui/config/config.go
+++ ui/config/config.go
@@ -8,20 +8,20 @@ import (
 
 // UIConfig for custom chart board
 type UIConfig struct {
-	MaxRow          int `toml:"max-row"`
-	SparklineHeight int `toml:"sp-height"`
+	MaxRow          int `toml:"max-chart-row"`
+	SparklineHeight int `toml:"chart-height"`
 }
 
 // Validate UIConfig
 func (c *UIConfig) Validate() error {
 	if c.MaxRow <= 0 {
 		return &errors.ConfigError{
-			Msg: fmt.Sprintf("max row of ui should be positive(max-row=%d)", c.MaxRow),
+			Msg: fmt.Sprintf("max row of chart should be positive(max-chart-row=%d)", c.MaxRow),
 		}
 	}
 	if c.SparklineHeight < 0 {
 		return &errors.ConfigError{
-			Msg: fmt.Sprintf("single chart line height should not be negative(sp-height=%d)", c.SparklineHeight),
+			Msg: fmt.Sprintf("single chart line height should not be negative(chart-height=%d)", c.SparklineHeight),
 		}
 	}
 	return nil
